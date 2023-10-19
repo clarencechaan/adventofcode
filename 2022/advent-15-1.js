8540fcde339fef213d47d2759b25fa83574a4825
Clarence Chan <clarencechaan@icloud.com>
2023-10-19 16:41:35 -0400

    Day 15

---
+++ 2022/advent-15-1.js
@@ -0,0 +1,72 @@
+let input = `Sensor at x=346753, y=3974683: closest beacon is at x=527500, y=3570474
+Sensor at x=3718952, y=2421864: closest beacon is at x=3871651, y=2935623
+Sensor at x=919295, y=2535231: closest beacon is at x=527500, y=3570474
+Sensor at x=3982082, y=2935124: closest beacon is at x=3871651, y=2935623
+Sensor at x=3693260, y=3095908: closest beacon is at x=3669901, y=3086819
+Sensor at x=3273954, y=2072378: closest beacon is at x=3275433, y=2000000
+Sensor at x=2936799, y=2402146: closest beacon is at x=2472215, y=2601723
+Sensor at x=3153125, y=3571786: closest beacon is at x=3669901, y=3086819
+Sensor at x=3892050, y=3718400: closest beacon is at x=3871651, y=2935623
+Sensor at x=1670487, y=618098: closest beacon is at x=-171619, y=497931
+Sensor at x=277848, y=3523156: closest beacon is at x=527500, y=3570474
+Sensor at x=3412305, y=198361: closest beacon is at x=3275433, y=2000000
+Sensor at x=2324630, y=2084344: closest beacon is at x=2472215, y=2601723
+Sensor at x=3718763, y=3088136: closest beacon is at x=3669901, y=3086819
+Sensor at x=303245, y=10539: closest beacon is at x=-171619, y=497931
+Sensor at x=3230426, y=1961497: closest beacon is at x=3275433, y=2000000
+Sensor at x=3616662, y=3171194: closest beacon is at x=3669901, y=3086819
+Sensor at x=3673367, y=3084002: closest beacon is at x=3669901, y=3086819
+Sensor at x=3350734, y=2116841: closest beacon is at x=3275433, y=2000000
+Sensor at x=1348476, y=3646233: closest beacon is at x=527500, y=3570474
+Sensor at x=2817552, y=1654754: closest beacon is at x=3275433, y=2000000
+Sensor at x=22462, y=1187401: closest beacon is at x=-171619, y=497931
+Sensor at x=3982255, y=1687776: closest beacon is at x=3275433, y=2000000
+Sensor at x=3995256, y=2821993: closest beacon is at x=3871651, y=2935623
+Sensor at x=501107, y=3777047: closest beacon is at x=527500, y=3570474
+Sensor at x=2507803, y=2761101: closest beacon is at x=2472215, y=2601723
+Sensor at x=2252560, y=2800057: closest beacon is at x=2370240, y=2923480
+Sensor at x=3536077, y=2843837: closest beacon is at x=3669901, y=3086819
+Sensor at x=533494, y=3224642: closest beacon is at x=527500, y=3570474
+Sensor at x=2962174, y=3016018: closest beacon is at x=2370240, y=2923480
+Sensor at x=177378, y=3319282: closest beacon is at x=527500, y=3570474
+Sensor at x=2074019, y=3934937: closest beacon is at x=2370240, y=2923480
+Sensor at x=2295953, y=3295493: closest beacon is at x=2370240, y=2923480`;
+
+input = input
+  .split("\n")
+  .map((str) => str.split(", "))
+  .map((arr) => ({
+    sx: parseInt(arr[0].slice(arr[0].indexOf("x=") + 2)),
+    sy: parseInt(arr[1].slice(2, arr[1].indexOf(":"))),
+    bx: parseInt(arr[1].slice(arr[1].indexOf("x=") + 2)),
+    by: parseInt(arr[2].slice(2)),
+  }));
+
+for (let sensor of input)
+  sensor.dist =
+    Math.abs(sensor.sx - sensor.bx) + Math.abs(sensor.sy - sensor.by);
+
+let minX = 0;
+let maxX = 0;
+let maxDist = 0;
+
+for (const { sx, bx, dist } of input) {
+  minX = Math.min(minX, sx, bx);
+  maxX = Math.max(maxX, sx, bx);
+  maxDist = Math.max(maxDist, dist);
+}
+
+let row = 2000000;
+let output = 0;
+for (let i = minX; i <= maxX + maxDist; i++) {
+  if (
+    input.every((sensor) => sensor.bx !== i || sensor.by !== row) &&
+    input.some(
+      (sensor) =>
+        Math.abs(row - sensor.sy) + Math.abs(i - sensor.sx) <= sensor.dist
+    )
+  )
+    output++;
+}
+
+console.log(output);
