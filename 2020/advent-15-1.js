255122bff6c81bdf7f869df0f0d59b08ceb2c9ab
Clarence Chan <clarencechaan@icloud.com>
2023-11-17 12:20:08 -0500

    Day 15 part 1

---
+++ 2020/advent-15-1.js
@@ -0,0 +1,17 @@
+let input = `11,0,1,10,5,19`;
+
+input = input.split(",").map((str) => parseInt(str));
+
+let turn = {};
+for (let i = 0; i < input.length; i++) turn[input[i]] = i;
+
+let prev = input[input.length - 1];
+for (let i = input.length; i < 2020; i++) {
+  let next;
+  if (turn[prev] === undefined) next = 0;
+  else next = i - turn[prev] - 1;
+  turn[prev] = i - 1;
+  prev = next;
+}
+
+console.log(prev);
