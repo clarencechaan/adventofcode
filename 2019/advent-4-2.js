1f4deda82046b300a3ab7055e2fbde94fa55c3b5
Clarence Chan <clarencechaan@icloud.com>
2024-01-04 12:50:02 -0500

    Day 4 part 2

---
+++ 2019/advent-4-2.js
@@ -0,0 +1,21 @@
+let input = `372037-905157`;
+
+input = input.split("-").map((str) => parseInt(str));
+
+let output = 0;
+loop: for (let password = input[0]; password <= input[1]; password++) {
+  let str = password.toString();
+  for (let i = 0; i < str.length - 1; i++)
+    if (str[i] > str[i + 1]) continue loop;
+  for (let i = 0; i < str.length - 1; i++)
+    if (
+      str[i] === str[i + 1] &&
+      str[i - 1] !== str[i] &&
+      str[i + 1] !== str[i + 2]
+    ) {
+      output++;
+      continue loop;
+    }
+}
+
+console.log(output);
